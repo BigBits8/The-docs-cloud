e9dc0c8a8f24eb936a273ca78ff2bb31742b959c
Mohammd Mustafa <momust187@gmail.com>
2022-01-03 21:49:09 +0100

    making of dynamic js

--- nextPage.js
+++ nextPage.js
@@ -1,19 +1,2 @@
-class doctor{
-    constructor(lastname, firstname, spec, info){
-        this.lastname = lastname;
-        this.firstname = firstname;
-        this.spec = spec;
-        this.info = info;
+const changeIt = document.querySelector('#nextPage2');
 
-        function printDoc(doc){
-            document.write("<h4>" + this.lastname + " " + this-firstname + "</h4>");
-            document.write("<h5>Area(s) of Specialization:<br>" + this.spec + "</h5>"+ "<br>");
-            document.write("<h5>" + this.info + "</h5>");
-
-        }
-    }
-}
-
-var doc1 = new doctor("Bloomberg", "Mark", "Psycologist", "Mark is a psycologist who studied at oxford university");
-var doc2 = new doctor("Bloomberg", "Mark", "Psycologist", "Mark is a psycologist who studied at oxford university");
-var doc3 = new doctor("Bloomberg", "Mark", "Psycologist", "Mark is a psycologist who studied at oxford university");
